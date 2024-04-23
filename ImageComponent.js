299cd834440bfc6ad6eb1db4a639e23220727306
aditya051312 <adityakhillari1@gmail.com>
2024-04-23 20:14:46 +0530

    Added ImageComponent.js file

---
+++ ImageComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ImageComponent() {
+  return (
+    <img src="path-to-image.jpg" alt="Login" className="img-fluid" />
+  );
+}
+
+export default ImageComponent;
