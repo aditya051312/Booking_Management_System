534522c75a607e21accb87a0528edbd7c8e67281
aditya051312 <adityakhillari1@gmail.com>
2024-05-10 09:12:59 +0530

    Added DashboardPage.js for the LMS login page

---
+++ DashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* TODO: Implement dashboard content here */}
+    </div>
+  );
+}
+
+export default DashboardPage;
