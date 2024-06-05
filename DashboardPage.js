9c4ce28ed9d71a4fdd74b5431487ef3c36b963f9
aditya051312 <adityakhillari1@gmail.com>
2024-06-05 12:24:12 +0530

    Added DashboardPage.js

---
+++ DashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div className="dashboard-page">
+      <h1>Welcome to the Dashboard</h1>
+      {/* Place your dashboard content here */}
+    </div>
+  );
+}
+
+export default DashboardPage;
