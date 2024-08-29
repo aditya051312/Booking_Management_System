f05160919b5af7248d06c6113a2bb5d504a2f90e
aditya051312 <adityakhillari1@gmail.com>
2024-08-29 07:57:01 +0530

    Added LoginPage.js for the login page of the Learning Management System.

---
+++ LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+import Image from './Image';
+import './LoginPage.css';
+
+function LoginPage() {
+  return (
+    <div className="login-page">
+      <Image />
+      <Form />
+    </div>
+  );
+}
+
+export default LoginPage;
