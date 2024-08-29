bbccc5b92b65a6e9c54e70244577533396a308b5
aditya051312 <adityakhillari1@gmail.com>
2024-08-29 07:58:02 +0530

    Added Image.js for displaying the image on the login page of the Learning Management System.

---
+++ Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Image.css';
+
+function Image() {
+  return (
+    <div className="image">
+      <img src="https://example.com/image.jpg" alt="Learning" />
+    </div>
+  );
+}
+
+export default Image;
